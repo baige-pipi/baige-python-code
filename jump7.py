420603b7df464db1d92871bf6387268903e6245c
baige-pipi <746122313@qq.com>
2019-08-24 00:01:48 +0800

    first commit

---
+++ jump7.py
@@ -0,0 +1,13 @@
+#!coding = UTF-8
+
+print ('''
+============================
+
+          JUMP7
+
+============================
+''')
+
+for num in range(1,101):
+    if num % 7 != 0 and num % 10 != 7 and num // 10 != 7:
+        print(num)
