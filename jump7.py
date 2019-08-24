679c4b844b0ebfb853e1139d09b6d28712e03476
baige <746122313@qq.com>
2019-08-24 08:37:20 +0800

    20190824

--- jump7.py
+++ jump7.py
@@ -8,6 +8,13 @@ print ('''
 ============================
 ''')
 
-for num in range(1,101):
-    if num % 7 != 0 and num % 10 != 7 and num // 10 != 7:
-        print(num)
+#for num in range(1,101):
+#    if num % 7 != 0 and num % 10 != 7 and num // 10 != 7:
+#        print(num)
+
+num = 1
+while num <= 100:
+	if num % 7 != 0 and num % 10 != 7 and num // 10 != 7:
+		print (num)
+	num = num + 1
+print ("DONE!")
